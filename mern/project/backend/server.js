84f60b1c4c62a94d78448eea4dbe6bc243cea98b
Nikita-Briskstar <nikitak.briskstar@gmail.com>
2022-10-10 18:57:47 +0530

    solve CORS

--- mern/project/backend/server.js
+++ mern/project/backend/server.js
@@ -3,6 +3,15 @@ const app = require('./app');
 const dotenv = require('dotenv');
 const connectDatabase = require('./config/database')
 
+const cors = require('cors');
+// var cors = require('cors');
+
+// const corsOptions ={
+//     origin:'http://localhost:3000', 
+//     credentials:true,            //access-control-allow-credentials:true
+//     optionSuccessStatus:200
+// }
+
 //config
 dotenv.config({path:'backend/config/config.env'});
 
@@ -11,3 +20,10 @@ connectDatabase()
 app.listen(process.env.PORT , () => {
     console.log(`server is on http://localhost:${process.env.PORT}`)
 })
+
+app.use(cors());
+
+// const cors = require('cors');
+
+
+// app.use(cors(corsOptions));
